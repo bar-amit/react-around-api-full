d7a84b0f775d21f9eb38a51d30b678a3ed5ec761
bar-amit <bar.amit@outlook.com>
2021-12-26 14:56:17 +0200

    style: lint

--- backend/utils/errors.js
+++ backend/utils/errors.js
@@ -1,8 +1,8 @@
-const BadRequestError = require("./error/BadRequestError");
-const UnauthorizedErorr = require("./error/UnauthorizedError");
-const ForbiddenError = require("./error/ForbiddenError");
-const NotFoundError = require("./error/NotFoundError");
-const ServerError = require("./error/ServerError");
+const BadRequestError = require('./error/BadRequestError');
+const UnauthorizedErorr = require('./error/UnauthorizedError');
+const ForbiddenError = require('./error/ForbiddenError');
+const NotFoundError = require('./error/NotFoundError');
+const ServerError = require('./error/ServerError');
 
 module.exports = {
   UnauthorizedErorr,
