43ecebb3e2c68037779b399b2a46369bf6aa98cc
bar-amit <bar.amit@outlook.com>
2021-12-26 14:56:57 +0200

    fix: set default port to 3000

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const { errors } = require('celebrate');
-const { usersRouter } = require('./routes/users');
-const { cardsRouter } = require('./routes/cards');
-const handleNotFound = require('./controllers/notFound');
-const { errorHandler } = require('./middlewares/errorHandler');
-const { errorLogger, requestLogger } = require('./middlewares/logger');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { errors } = require("celebrate");
+const { usersRouter } = require("./routes/users");
+const { cardsRouter } = require("./routes/cards");
+const handleNotFound = require("./controllers/notFound");
+const { errorHandler } = require("./middlewares/errorHandler");
+const { errorLogger, requestLogger } = require("./middlewares/logger");
 
 const app = express();
 
-const { PORT = 3001, DB = 'mongodb://localhost:27017/aroundb' } = process.env;
+const { PORT = 3000, DB = "mongodb://localhost:27017/aroundb" } = process.env;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -27,4 +27,4 @@ app.use(errorHandler);
 
 mongoose.connect(DB);
 
-app.listen(PORT, 'localhost');
+app.listen(PORT, "localhost");
