a72547b5770dbe624e9ed26d4b779d5aa72a8d5f
bar-amit <bar.amit@outlook.com>
2021-12-28 11:44:12 +0200

    fix: error handler, message format

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 module.exports.errorHandler = (err, req, res, next) => {
-  if (err.statusCode && err.statusCode !== 500) res.status(err.status).send(err.message);
-  else res.status(500).send(new Error('Server error'));
+  if (err.statusCode && err.statusCode !== 500) {
+    res.status(err.statusCode).send({ message: err.message });
+  } else res.status(500).send(new Error('An error has occurred on the server.'));
 };
